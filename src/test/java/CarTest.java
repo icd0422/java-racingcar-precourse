d25890e73c1d421e19ee3a7d1972a76b5eb17d7a
장준희 <junheejang@wemakeprice.com>
2021-04-28 21:23:59 +0900

    refactor: Position클래스 음수값 생성 예외 테스트 코드 검증 추가

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -11,7 +11,10 @@ class CarTest {
     }
 
     @Test
-    void Postion_클래스_음수값_생성_예외처리_테스트() {
+    void Position_클래스_음수값_생성_예외처리_테스트() {
+        assertThat(new Position(0).getCurrentPos()).isZero();
+        assertThat(new Position(1).getCurrentPos()).isEqualTo(1);
+
         assertThatThrownBy(() -> new Position(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageStartingWith(Position.NOT_POSITIVE_VALUE_ERR_MSG);
